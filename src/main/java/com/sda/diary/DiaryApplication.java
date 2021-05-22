ca3d9475027400e1426c30efc14e3fd0ad5a8749
Michał <bakaro.sosnowa@gmail.com>
2021-05-22 11:51:50 +0200

    Initial commit

---
+++ src/main/java/com/sda/diary/DiaryApplication.java
@@ -0,0 +1,14 @@
+package com.sda.diary;
+
+public class DiaryApplication {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
+
+// SKRÓTY KLAWISZOWE
+
+// ALT + 1 - przeskakuje między okienkami Projects a kod
+// ALT + INSERT - tworzenie klasy, enum, package itd...
+// ALT + SHIFT + F10 - start aplikacji + wybór konfuguracji
+//
